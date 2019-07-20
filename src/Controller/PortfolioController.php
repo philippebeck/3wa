6c04e962c4d406282a7aae3ee9cf7441c66fc01f
Philippe Beck <philippe@philippebeck.net>
2019-07-20 22:53:02 +0200

    Refactoring PortfolioController

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -1,35 +1,32 @@
 <?php
 
-// ******************************** \\
-// ***** PORTFOLIO CONTROLLER ***** \\
-// ******************************** \\
-
 namespace App\Controller;
 
 use Pam\Controller\Controller;
 use Pam\Model\ModelFactory;
-use Pam\Helper\Session;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
-
-/** **********************************\
-* All control actions to the portfolio
-*/
+/**
+ * Class PortfolioController
+ * @package App\Controller
+ */
 class PortfolioController extends Controller
 {
-
-  /** ***********************\
-  * Render the portfolio view
-  * @return mixed => the rendering of the view portfolio
-  */
-  public function IndexAction()
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function indexAction()
   {
-    // Reads all projects, pens, routes & courses from the Portfolio, then stores them
     $allProjects = ModelFactory::get('Project')->list();
     $allPens     = ModelFactory::get('Pen')    ->list();
     $allRoutes   = ModelFactory::get('Route')  ->list();
     $allCourses  = ModelFactory::get('Course') ->list();
 
-    // Returns the rendering of the view portfolio
     return $this->render('portfolio/portfolio.twig', [
       'allProjects' => $allProjects,
       'allPens'     => $allPens,
