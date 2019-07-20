45343be0b036dd59381f5e63626f3cdd294ed999
Philippe Beck <philippe@philippebeck.net>
2019-07-20 22:35:10 +0200

    Refactoring the CommentController

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,69 +1,41 @@
 <?php
 
-// ****************************** \\
-// ***** COMMENT CONTROLLER ***** \\
-// ****************************** \\
-
 namespace App\Controller;
 
 use Pam\Controller\Controller;
 use Pam\Model\ModelFactory;
-use Pam\Helper\Session;
 
-
-/** *********************************\
-* All control actions to the comments
-*/
+/**
+ * Class CommentController
+ * @package App\Controller
+ */
 class CommentController extends Controller
 {
-
-  /** ********************\
-   * Creates a new comment
-   */
-  public function CreateAction()
-  {
-    // Checks if the user is connected
-    if (Session::islogged())
+    public function CreateAction()
     {
-      // Creates the $data array to store the new comment
-      $data['article_id']   = $_GET['id'];
-      $data['content']      = $_POST['content'];
-      $data['created_date'] = $_POST['date'];
-      $data['user_id']      = $_SESSION['user']['id'];
+        if ($this->session->islogged()) {
 
-      // Creates the comment
-      ModelFactory::get('Comment')->create($data);
+            $data['article_id']   = $this->get->getGetVar('id');
+            $data['content']      = $this->post->getPostVar('content');
+            $data['created_date'] = $this->post->getPostVar('date');
+            $data['user_id']      = $this->session->userId();
 
-      // Creates a valid message to confirm the creation of a new comment
-      htmlspecialchars(Session::createAlert('Nouveau commentaire créé avec succès !', 'valid'));
+            ModelFactory::get('Comment')->create($data);
 
-      // Redirects to the view readArticle
-      $this->redirect('article!read', ['id' => $_GET['id']]);
+            $this->cookie->createAlert('Nouveau commentaire créé avec succès !');
+
+            $this->redirect('article!read', ['id' => $this->get->getGetVar('id')]);
+        }
+        $this->cookie->createAlert('Vous devez être connecté pour ajouter un commentaire...');
+
+        $this->redirect('user!login');
     }
-    else {
-      // Creates a fail message to ask to be connected
-      htmlspecialchars(Session::createAlert('Vous devez être connecté pour ajouter un commentaire...', 'cancel'));
+
+    public function DeleteAction()
+    {
+        ModelFactory::get('Comment')->delete($this->get->getGetVar('id'));
+        $this->cookie->createAlert('Commentaire définitivement supprimé !');
+
+        $this->redirect('admin');
     }
-    // Redirects to the view loginUser
-    $this->redirect('user!login');
-  }
-
-
-  /** ****************\
-   * Deletes a comment
-   */
-  public function DeleteAction()
-  {
-    // Gets the comment id, then stores it
-    $id = $_GET['id'];
-
-    // Deletes the selected comment
-    ModelFactory::get('Comment')->delete($id);
-
-    // Creates a delete message to confirm the removal of the selected comment
-    htmlspecialchars(Session::createAlert('Commentaire définitivement supprimé !', 'delete'));
-
-    // Redirects to the view admin
-    $this->redirect('admin');
-  }
 }
