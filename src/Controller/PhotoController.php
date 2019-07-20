fa1420a3c4430c6c8e55976549f090f58ed88c07
Philippe Beck <philippe@philippebeck.net>
2019-07-20 22:50:38 +0200

    Refactoring PhotoController

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -1,53 +1,46 @@
 <?php
 
-// **************************** \\
-// ***** PHOTO CONTROLLER ***** \\
-// **************************** \\
-
 namespace App\Controller;
 
 use Pam\Controller\Controller;
 use Pam\Model\ModelFactory;
-use Pam\Helper\Session;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
-
-/** *******************************\
-* All control actions to the photos
-*/
+/**
+ * Class PhotoController
+ * @package App\Controller
+ */
 class PhotoController extends Controller
 {
-
-  /** **************\
-  * Reads all photos
-  * @return mixed => the rendering of the view photos
-  */
-  public function IndexAction()
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function indexAction()
   {
-    // Reads all photos, then stores them
     $allPhotos = ModelFactory::get('Photo')->list();
 
-    // Returns the rendering of the view photo with all photos
     return $this->render('photo/photos.twig', [
       'allPhotos' => $allPhotos
     ]);
   }
 
-
-  /** *******************************************\
-  * Reads a photo, his object & his constellation
-  * @return mixed => the rendering of the view readPhoto
-  */
-  public function ReadAction()
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function ReadAction()
   {
-    // Gets the photo id, then stores it
-    $id = $_GET['id'];
-
-    // Reads the photo, his object & his constellation, then stores them
-    $photo      = ModelFactory::get('Photo')        ->read($id);
-    $photoObj   = ModelFactory::get('Object')       ->read($photo['object_id']);
+    $photo      = ModelFactory::get('Photo')->read($this->get->getGetVar('id'));
+    $photoObj   = ModelFactory::get('Object')->read($photo['object_id']);
     $photoConst = ModelFactory::get('Constellation')->read($photo['const_id']);
 
-    // Returns the rendering of the view readPhoto with the current photo, his object & his constellation
     return $this->render('photo/readPhoto.twig', [
       'photo'      => $photo,
       'photoObj'   => $photoObj,
